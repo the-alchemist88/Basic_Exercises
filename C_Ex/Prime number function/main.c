e1ce94c8e9cb170ca75ecdd66173b216c9f59160
Berk <berktopbas@gmail.com>
2022-09-03 13:11:40 +0300

    Add C&C++ Quiz

--- C_Ex/Prime number function/main.c
+++ C_Ex/Prime number function/main.c
@@ -15,13 +15,13 @@ void prime_number(int x){ //Function definition
     if(x==2)//adding exception of the smallest prime number
         printf("2 is a prime number");
     for(int i=2;i<x;i++){//Checks the number until i<x
-            if(x%i==0){//If remainder of division is equal to 0, print it and then break the for loop
-            printf("%d is not a prime number",x);
-            break;
-            }
-            else if(i<(x-1))//Skip printing until i<(x-1), if it still meets the condition
-            continue;
-            else//So it's a prime number, print it and for loop ends
-            printf("%d is a prime number",x);
+		if(x%i==0){//If remainder of division is equal to 0, print it and then break the for loop
+			printf("%d is not a prime number",x);
+			break;
+		}
+		else if(i<(x-1))//Skip printing until i<(x-1), if it still meets the condition
+			continue;
+		else//So it's a prime number, print it and for loop ends
+			printf("%d is a prime number",x);
     }
 }
